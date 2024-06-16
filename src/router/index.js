2e1984ac58ed9cd87f8a6b150e515b134af5f555
MarwanMaher0 <Marwanmaher635@gmail.com>
2024-06-16 11:36:07 +0300

    setup-done

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "Gym",
+      component: () => import("../views/index.vue"),
+    },
+    // auth
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("../views/auth/login.vue"),
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: () => import("../views/auth/register.vue"),
+    },
+    // ecom
+    {
+      path: "/ecom",
+      name: "Ecom",
+      component: () => import("../views/ecom/index.vue"),
+    },
+    {
+      path: "/ecom/cart",
+      name: "Cart",
+      component: () => import("../views/ecom/cart.vue"),
+    },
+    {
+      path: "/ecom/wishlist",
+      name: "Wishlist",
+      component: () => import("../views/ecom/Wishlist.vue"),
+    },
+  ],
+});
+
+export default router;
